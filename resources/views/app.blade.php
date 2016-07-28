d3818f3d37cfac56c61eb6e37bb6cc398288c02b
Lars Gaubisch <lars.gaubisch@tausendkind.de>
2016-07-28 12:36:14 +0200

    some small optimizations for development environment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 <!--	{!! HTML::style('css/main.css') !!}-->
 	<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-	{!! HTML::style('css/laravel.css') !!}
+	{!! HTML::style('assets/css/laravel.css') !!}
 	{!! HTML::script('assets/js/app.min.js') !!}
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
