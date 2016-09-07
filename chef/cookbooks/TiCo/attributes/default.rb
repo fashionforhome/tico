207e858a9baaa493ade8ab74d1b75e63ac1467ee
Rebel-L <dj@rebel-l.net>
2016-09-07 15:15:48 +0200

    #7: reorganized build files and created a copy chef cookbook task. also finished the configuration stuff in TiCo cookbook

--- chef/cookbooks/TiCo/attributes/default.rb
+++ chef/cookbooks/TiCo/attributes/default.rb
@@ -8,8 +8,16 @@ default['TiCo']['database']['user']     = 'homestead'
 default['TiCo']['database']['password'] = 'secret'
 
 # Jira
-default['TiCo']['jira']['user']         = ''
-default['TiCo']['jira']['password']     = ''
+default['TiCo']['jira']['user']                         = ''
+default['TiCo']['jira']['password']                     = ''
+default['TiCo']['jira']['baseUrl']                      = ''
+default['TiCo']['jira']['customFields']['devTeam']      = ''
+default['TiCo']['jira']['customFields']['epic']         = ''
+default['TiCo']['jira']['customFields']['sprintName']   = ''
+default['TiCo']['jira']['customFields']['storyPoints']  = ''
 
 # FOP
 default['TiCo']['fop']['path'] = '/usr/bin/fop'
+
+# Printer
+default['TiCo']['printer']['name']  = 'ticketprinter'
--- chef/cookbooks/TiCo/recipes/default.rb
+++ chef/cookbooks/TiCo/recipes/default.rb
@@ -16,3 +16,23 @@ template "#{node['TiCo']['rootFolder']}/.env" do
 	owner "root"
 	group "root"
 end
+
+#
+# Create jira config
+#
+template "#{node['TiCo']['rootFolder']}/config/jira.php" do
+	source "config/jira.php.erb"
+	mode "0644"
+	owner "root"
+	group "root"
+end
+
+#
+# Create printer config
+#
+template "#{node['TiCo']['rootFolder']}/config/printer.php" do
+	source "config/printer.php.erb"
+	mode "0644"
+	owner "root"
+	group "root"
+end
